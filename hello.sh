950ef245398a466b7be7a6b985124e46ed4a4321
kaubarros-24 <martinsbarroskaua@gmail.com>
2025-01-02 10:02:26 -0300

    chore: learn variables with bash

--- hello.sh
+++ hello.sh
@@ -1,3 +1,14 @@
 #! /bin/bash
 
-echo "hello world"
+#Hi, I'm comment
+echo "hello world" #I'm too a comment
+
+name="Kauã"
+echo My name is $name
+name="Your name is João"
+echo $name
+echo Our shell name is $BASH
+
+value=10
+
+echo $value + 1
